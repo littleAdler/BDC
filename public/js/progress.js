153dd32815941b72e923dc7444b24634d749264d
littleAdler <mspouge@gmail.com>
2018-12-17 16:13:20 -0800

    More databse query support

--- public/js/progress.js
+++ public/js/progress.js
@@ -1,7 +1,12 @@
 $('.collapsedmid').click(function(){
-  $.get('/progress',function(data,status){
-    console.log(data);
-    console.log(status);
-  })
 
+  //Query the db for the user info and render everything onto the page
+
+  $.get('/progress',function(data,status){
+    //console.log(data);
+    //console.log(status);
+    console.log(data[0].studentlevel);
+    $('#level').text(data[0].studentlevel);
+
+  })
 });
